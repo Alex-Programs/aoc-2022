135e6cfcc9eda61e486ca73b7274889100bd73f6
AlexAndHisScripts <alexcj2802@gmail.com>
2022-12-06 08:33:40 +0000

    No selection

--- 06/noselection.py
+++ 06/noselection.py
@@ -1,6 +1,3 @@
-# Doesn't work
-# Need to make it stop overwriting the index
-
 with open("input-prod.txt") as f:
     data = f.read().split("\n")
 
@@ -8,11 +5,20 @@ data = data[0]
 
 index = 0
 
+def has_duplicates(l):
+    duplicateCount = 0
+    for i in l:
+        for j in l:
+            duplicateCount += (i == j)
+
+    return duplicateCount > len(l)
+
+
 for i, char in enumerate(data):
-    isShorter = int(len(set(data[i - 14:i])) == len(data[i - 14:i]))
-    isZero = int(index == 0)
+    isNotDuplicate = not has_duplicates(data[i - 14:i])  # int(len(set(data[i - 14:i])) == len(data[i - 14:i]))
+    isIndexZero = int(index == 0)
     greaterThan14 = int(i > 14)
 
-    index = index + ((isShorter * i) * isZero * greaterThan14)
+    index = index + ((isNotDuplicate * i) * isIndexZero * greaterThan14)
 
 print(index, data[index])
